bb8c81416cee66ba39eb033a8b871984f4b94a46
Gaspard FEREY <gaspard.ferey@gmail.com>
2025-01-08 13:45:15 +0100

    New just-fail pre-commit

---
+++ pre_commit_hooks/just_fail.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import argparse
+from collections.abc import Sequence
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*', help='Filenames to fix')
+    parser.add_argument(
+        '--message',
+        type=str,
+        dest='message',
+        help='Warning message to emit',
+        default='You are trying to push modifications to a protected file. If everything else looks fine and you know what you are doing, use `--no-verify` to skip all checks.',
+    )
+    args = parser.parse_args(argv)
+    print(f"{args.message}")
+    return 1
+
+if __name__ == "__main__":
+    raise SystemExit(main())
